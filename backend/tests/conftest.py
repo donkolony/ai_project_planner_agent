0d9e64952aecd7149d88dc787a7629e863388748
Yamkela macwili <yamkela22y@gmail.com>
2026-03-11 09:47:36 +0200

    Cleanup: Removed unused code

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,10 +1,6 @@
 import pytest
 from sqlmodel import SQLModel, Session, create_engine
-from app.models.db_models import PlanDB
-from app.core.database import get_session
 from app.main import app
-from tests.mocks.mock_ai_planner import MockAIPlanner
-# from app.api.planner import get_ai_planner
 
 # In-memory SQLite for testing
 TEST_SQLITE_URL = "sqlite:///./test.db"
@@ -31,9 +27,3 @@ def get_test_session():
 def get_test_session_override():
     with Session(engine) as session:
         yield session
-
-# Automatically override FastAPI dependency
-# @pytest.fixture(scope="session", autouse=True)
-# def override_app_session():
-#     app.dependency_overrides[get_session] = get_test_session_override
-#     app.dependency_overrides[get_ai_planner] = lambda: MockAIPlanner()
